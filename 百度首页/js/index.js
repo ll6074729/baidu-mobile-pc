14b8e905b7545887daa64d7077299e691aabd121
ll6074729 <6074729@qq.com>
2017-03-28 14:28:57 +0800

    Add files via upload

---
+++ 百度首页/js/index.js
@@ -0,0 +1,33 @@
+
+// 获取ID
+var aside = document.getElementById("aside");
+var More = document.getElementById("More");
+// 失去焦点
+More.onmouseout =  function(){
+    aside.style.display = 'none';
+}
+// 获得焦点
+More.onmouseover =  function(){
+    aside.style.display = 'block';
+}
+// 失去焦点
+aside.onmouseout =  function(){
+    this.style.display = 'none';
+}
+// 获得焦点
+aside.onmouseover =  function(){
+    this.style.display = 'block';
+}
+//设置
+var set = document.getElementById("set");
+var setPf = document.getElementById("set-pf");
+var pf = document.getElementById("pf");
+
+set.onmouseover = function(){
+	setPf.style.display='block';
+
+}
+setPf.onmouseout = function(){
+	// console.log("aaa");
+	setPf.style.display='none';
+}
